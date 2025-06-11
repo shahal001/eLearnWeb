da08119028030d98d5c5a69a116b869512e2fb53
shahal001 <--replace-all>
2025-06-11 15:24:38 +0530

    tailwind css added

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
